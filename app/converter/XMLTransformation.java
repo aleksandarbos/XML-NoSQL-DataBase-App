a0dc407249a70e6a36d0aca898c4b4fe991a3977
aleksandarbos <aleksandarbos.ns@gmail.com>
2016-06-13 18:46:04 +0200

    XMLTransformation changed imports, and fop constructor.

--- app/converter/XMLTransformation.java
+++ app/converter/XMLTransformation.java
@@ -1,31 +1,27 @@
 package converter;
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
+import org.apache.fop.apps.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.transform.*;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.*;
 
 
 public class XMLTransformation {
 
-	private static void transformToPdf() {
+	private static void transformToPdf() throws SAXException, TransformerException, IOException {
 		
 		File xmlFile = new File("xml_schema/primeri/amandment.xml");
 		File pdfFile = new File("xml_schema/primeri/amandment.pdf");
 		File xsltFile = new File("conf/to_pdf.xsl");
 
-		FopFactory fopFactory = FopFactory.newInstance(new File("conf/fop.xconf"));
+		FopFactory fopFactory = FopFactory.newInstance();
+		fopFactory.setUserConfig(new File("conf/fop.xconf"));
 		TransformerFactory transformerFactory = new TransformerFactoryImpl();
 
 		System.out.println("[INFO] Transformation to PDF: Started.");
