d0f6bad270e6849d67f4430ac4bbf43eb7090665
aleksandarbos <aleksandarbos.ns@gmail.com>
2016-06-14 16:17:49 +0200

    Edited Amendments controller and view.

--- app/controllers/Amendments.java
+++ app/controllers/Amendments.java
@@ -3,24 +3,34 @@ package controllers;
 import converter.Converter;
 import converter.types.UnmarshallType;
 import dal.AmendmentsDAO;
+import dal.RegulationsDAO;
 import models.rs.gov.parlament.amandmani.Amandman;
+import models.rs.gov.parlament.propisi.Propis;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
 
 import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
 
 @With(Secure.class)
 public class Amendments extends Controller {
 
-    public static void show() {
+    public static void show() throws IOException, JAXBException {
         String userType = session.get("user-type");
-    	render(userType);
+
+        HashMap<String, Propis> regulationsSearch = RegulationsDAO.fetchAllRegulations();
+        Collection<Propis> regulations = regulationsSearch.values();
+        render(regulations);
+
+    	// render(userType);
     }
 
     public static void create(String amandmentName, String affectedRegulationUri,
                               int affectedClause, String affectedType, String user,
-                              String amendmentContent) throws JAXBException {
+                              String amendmentContent) throws JAXBException, IOException {
 
         System.out.println("dodaj akt: " + amandmentName + ", predlozio korisnik: " + user + ", sadrzaj dokumenta: "+amendmentContent+", menja se dokument sa id: "+affectedRegulationUri+", id clana: "+affectedClause+", tip izmene "+affectedType);
 
